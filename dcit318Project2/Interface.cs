bfa9907a905bc4a088d5ebb0bd91185475c96c6c
Joel Jeswon Tetteh <jjtetteh001@st.ug.edu.gh>
2025-08-05 16:08:29 +0000

    Added Airplane class implementing IMovable interface

--- dcit318Project2/Interface.cs
+++ dcit318Project2/Interface.cs
@@ -21,14 +21,24 @@ class Bicycle : IMovable
     }
 }
 
-class Program
+class Airplane : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Airplane is flying");
+    }
+}
+
+class Interface
 {
     static void Main(string[] args)
     {
         Car car = new Car();
         Bicycle bicycle = new Bicycle();
+        Airplane airplane = new Airplane();
 
         car.Move();
         bicycle.Move();
+        airplane.Move();
     }
 }
